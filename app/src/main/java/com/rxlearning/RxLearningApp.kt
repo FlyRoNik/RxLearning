500da0db0d71f7f8f692940b956ef74b5de3da84
FlyRoNik <hang.glider.viru007@gmail.com>
2018-10-04 21:36:47 +0300

    add Crashlytics

--- app/src/main/java/com/rxlearning/RxLearningApp.kt
+++ app/src/main/java/com/rxlearning/RxLearningApp.kt
@@ -1,6 +1,21 @@
 package com.rxlearning
 
 import android.app.Application
+import com.crashlytics.android.Crashlytics
+import io.fabric.sdk.android.Fabric
 
 class RxLearningApp : Application() {
+
+    companion object {
+        lateinit var instance: RxLearningApp
+            private set
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+        if (BuildConfig.DEBUG) Fabric.with(this, Crashlytics())
+//        DatabaseCreator.createDb(this)
+//        Stetho.initializeWithDefaults(this)
+    }
 }
